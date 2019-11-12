d7458a6b65cf596dcac941c576881e3f97abea52
emeric <marion.emeric@laposte.net>
2019-11-12 18:04:21 +0100

    03 - Symfony : Création de la page d’accueil

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index() : Response
+    {
+        return $this->render('home.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
