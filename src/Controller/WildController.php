3692064045e65a93607b1dfacb0f0f0500d08bf5
emeric <marion.emeric@laposte.net>
2019-11-20 14:29:23 +0100

    Quest : 05 - Symfony : Le routing avancé

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,19 +2,31 @@
 // src/Controller/WildController.php
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 Class WildController extends AbstractController
 {
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index() : Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("show/{slug}", requirements={"slug"="[a-z0-9-]+"}, defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"}, name="wild_show")
+     * @param string $slug
+     * @return Response
+     */
+    public function show(string $slug): Response
+    {
+        $newSlug = ucwords(str_replace("-", " ", $slug));
+        return $this->render('wild/show.html.twig', ['newSlug' => $newSlug]);
     }
 }
