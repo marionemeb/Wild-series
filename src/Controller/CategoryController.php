4d9d457805d8005654016abf1e07e056d0e1d963
emeric <marion.emeric@laposte.net>
2019-11-28 00:19:26 +0100

    11 - Symfony : Gardez la “form” ! v2

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,6 +7,7 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,40 +15,48 @@ class CategoryController extends AbstractController
 {
     /**
      * @Route("/form", name="formCategory")
+     * @param Request $request
+     * @return Response
      */
-    public function category()
+    public function category(Request $request): Response
     {
         $form = $this
             ->createForm(CategoryType::class);
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+            $data->setName('name');
+            $request->persist($data);
+            $request->flush();
+        }
 
         return $this->render('form/categoryForm.html.twig', [
             'form' => $form->createView()
         ]);
     }
 
-    /**
-     * @Route("/form", name="addCategory")
-     * @param EntityManagerInterface $entityManager
-     * @return Response
-     */
-    public function add(EntityManagerInterface $entityManager) :Response
-    {
-        // declare a new category
-        $category = new Category();
-
-        // set category properties
-        $category->setName('name');
-
-        // declare object to doctrine (no SQL query is done)
-        $entityManager->persist($category);
-
-        // execute SQL query
-        // for this object but also all doctrine objects of the script
-        $entityManager->flush();
-
-        return $this->render('form/categoryForm.html.twig', [
-            'category' => $category,
-        ]);
-    }
+//    /**
+//     * @Route("/form", name="addCategory")
+//     * @param EntityManagerInterface $entityManager
+//     * @return Response
+//     */
+//    public function add(Request $request): Response
+//    {
+//        // declare a new category
+//        $category = new Category();
+//
+//        // set category properties
+//        $category->setName('name');
+//
+//        // declare object to doctrine (no SQL query is done)
+//        $entityManager->persist($category);
+//
+//        // execute SQL query
+//        // for this object but also all doctrine objects of the script
+//        $entityManager->flush();
+//
+//        return $this->render('form/categoryForm.html.twig', [
+//            'category' => $category,
+//        ]);
+//    }
 
 }
